3fb7baf81b6091db450b11af805fecca1fc6c0c3
EE <eb_genius@mail.ru>
2018-07-19 23:54:52 +0400

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+const express = require('express');
+const path = require('path');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = require('./app');
+const server = require('./server');
+
+const secret = require('./pass').generate(15);
+const session = require('express-session')({
+    key: 'user_sid',
+    secret: secret,
+    resave: false,
+    saveUninitialized: false
+});
+
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(express.json())
+    .use(session)
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+;
+
+
+server
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+;
+
