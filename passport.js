1d6f376be69b826a38cd83b691e00698feeed18c
Evgenii Evgenevich <eb_genius@mail.ru>
2018-07-22 11:29:06 +0300

    +

--- passport.js
+++ passport.js
@@ -2,11 +2,11 @@ const server = require('./server');
 
 const GOOGLE_ID = process.env.PASSPORT_GOOGLE_ID || 'clientID';
 const GOOGLE_SECRET = process.env.PASSPORT_GOOGLE_SECRET || 'clientSecret';
-const GOOGLE_CALLBACK = server.address().address + ':' server.address().port + '/auth/google/callback';
+const GOOGLE_CALLBACK = server.address().address + ':' + server.address().port + '/auth/google/callback';
 
 const FACEBOOK_ID = process.env.PASSPORT_FACEBOOK_ID || 'clientID';
 const FACEBOOK_SECRET = process.env.PASSPORT_FACEBOOK_SECRET || 'clientSecret';
-const FACEBOOK_CALLBACK = server.address().address + ':' server.address().port + '/auth/facebook/callback';
+const FACEBOOK_CALLBACK = server.address().address + ':' + server.address().port + '/auth/facebook/callback';
 
 const host = server.address().address;
 const port = server.address().port;
